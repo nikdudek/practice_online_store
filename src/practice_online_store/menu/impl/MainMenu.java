e8c38c8abfeac1216cdf71ff3416ac5a5080af80
nikdudek <nik.dudek@gmail.com>
2024-10-07 22:42:04 +0200

    Added SingUpMenu and SingOutMenu classes. Implemented the MainMenu class.

--- src/practice_online_store/menu/impl/MainMenu.java
+++ src/practice_online_store/menu/impl/MainMenu.java
@@ -1,5 +1,8 @@
 package practice_online_store.menu.impl;
 
+import java.util.Scanner;
+
+import practice_online_store.Main;
 import practice_online_store.configs.ApplicationContext;
 import practice_online_store.menu.Menu;
 
@@ -27,13 +30,67 @@ public class MainMenu implements Menu {
 	
 	@Override
 	public void start() {
-		// <write your code here>
+		while (true) {
+			if(context.getMainMenu() == null) {
+				context.setMainMenu(this); //setting the context.mainMenu to this current instance of Main Menu
+			}
+			
+			Menu menuToNavigate = null;
+			mainLoop: while (true) {
+				printMenuHeader();
+				
+				Scanner sc = new Scanner(System.in);
+				
+				System.out.print("User Input: ");
+				String userInput = sc.next();
+				if (userInput.equalsIgnoreCase(Main.EXIT_COMMAND)) {
+					System.exit(0);
+				} else {
+					int commandNumber = Integer.parseInt(userInput);
+					switch (commandNumber) {
+					
+					case 1:
+						menuToNavigate = new SingUpMenu();
+						break mainLoop;
+					case 2:
+						if (context.getLoggedInUser() == null) {
+							menuToNavigate = new SignInMenu();
+						} else {
+							menuToNavigate = new SingOutMenu();
+						}
+						break mainLoop;
+					case 3:
+						menuToNavigate = new ProductCatalogMenu();
+						break mainLoop;
+					case 4:
+						menuToNavigate = new MyOrdersMenu();
+						break mainLoop;
+					case 5:
+						menuToNavigate = new SettingsMenu();
+						break mainLoop;
+					case 6:
+						menuToNavigate = new CustomerListMenu();
+						break mainLoop;
+					default:
+						System.out.println("Only 1, 2, 3, 4, 5 is allowed. Try one more time");
+						continue;
+					}
+				}
+			}
+			
+			menuToNavigate.start();
+		}
 		
 	}
 
 	@Override
 	public void printMenuHeader() {
-		// <write your code here>
+		System.out.println("----- MAIN MENU -----");
+		if (context.getLoggedInUser() == null) {
+			System.out.println(MAIN_MENU_TEXT_FOR_LOGGED_OUT_USER);
+		} else {
+			System.out.println(MAIN_MENU_TEXT_FOR_LOGGED_IN_USER);
+		}
 	}
 
 }
---
+++ src/practice_online_store/menu/impl/SingOutMenu.java
@@ -0,0 +1,19 @@
+package practice_online_store.menu.impl;
+
+import practice_online_store.menu.Menu;
+
+public class SingOutMenu implements Menu {
+
+	@Override
+	public void start() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void printMenuHeader() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
