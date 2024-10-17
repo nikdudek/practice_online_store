aeecc5c0d84d99d5d4d5bf3d11de0185181809d4
nikdudek <nik.dudek@gmail.com>
2024-10-17 08:21:00 +0200

    Implemented SettingsMenu class.

--- src/practice_online_store/menu/impl/SettingsMenu.java
+++ src/practice_online_store/menu/impl/SettingsMenu.java
@@ -1,5 +1,7 @@
 package practice_online_store.menu.impl;
 
+import java.util.Scanner;
+
 import practice_online_store.configs.ApplicationContext;
 import practice_online_store.menu.Menu;
 
@@ -16,12 +18,45 @@ public class SettingsMenu implements Menu {
 
 	@Override
 	public void start() {
-		// <write your code here>
+		Menu menuToNavigate = null;
+		mainLoop: while (true) {
+			printMenuHeader();
+			if (context.getLoggedInUser() == null) {
+				System.out.println("Please, log in or create new account to change your account settings");
+				new MainMenu().start();
+				return;
+			} else {
+				System.out.println(SETTINGS);
+				System.out.print("Please, enter option or type 'menu' to navigate back to main menu: ");
+				Scanner sc = new Scanner(System.in);
+				String userInput = sc.next();
+				
+				if (userInput.equalsIgnoreCase(MainMenu.MENU_COMMAND)) {
+					menuToNavigate = new MainMenu();
+					break mainLoop;
+				}
+				
+				int userOption = Integer.parseInt(userInput);
+				switch (userOption) {
+				case 1:
+					menuToNavigate = new ChangePasswordMenu();
+					break mainLoop;
+				case 2:
+					menuToNavigate = new ChangeEmailMenu();
+					break mainLoop;
+				default:
+					System.out.println("Only 1, 2 is allowed. Try one more time.");
+					continue;
+				}
+			}
+		}
+		
+		menuToNavigate.start();
 	}
 
 	@Override
 	public void printMenuHeader() {
-		// <write your code here>	
+		System.out.println("***** SETTINGS *****");
 	}
 
 }
