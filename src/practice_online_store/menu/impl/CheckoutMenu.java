94d5066bde90394eda22eead31d0e0970a53436d
nikdudek <nik.dudek@gmail.com>
2024-10-23 22:46:28 +0200

    Implemented CheckoutMenu.java

--- src/practice_online_store/menu/impl/CheckoutMenu.java
+++ src/practice_online_store/menu/impl/CheckoutMenu.java
@@ -1,6 +1,10 @@
 package practice_online_store.menu.impl;
 
+import java.util.Scanner;
+
 import practice_online_store.configs.ApplicationContext;
+import practice_online_store.enteties.Order;
+import practice_online_store.enteties.impl.DefaultOrder;
 import practice_online_store.menu.Menu;
 import practice_online_store.services.OrderManagementService;
 import practice_online_store.services.impl.DefaultOrderManagementService;
@@ -17,12 +21,38 @@ public class CheckoutMenu implements Menu {
 	
 	@Override
 	public void start() {
-		// <write your code here>
+		while (true) {
+			printMenuHeader();
+			Scanner sc = new Scanner(System.in);
+			String userInput = sc.next();
+			
+			if (!createOrder(userInput)) {
+				continue;
+			}
+			context.getSessionCart().clear();
+			break;
+		}
+		
+		System.out.println("Thanks a lot for your purchase. Details about order delivery are sent to your email.");
+	}
+	
+	private boolean createOrder(String creditCardNumber) {
+		Order order = new DefaultOrder();
+		if (!order.isCreditCardNumberValid(creditCardNumber)) {
+			return false;
+		}
+		
+		order.setCreditCardNumber(creditCardNumber);
+		order.setProducts(context.getSessionCart().getProducts());;
+		order.setCustomerId(context.getLoggedInUser().getId());
+		orderManagementService.addOrder(order);
+		return true;
 	}
 
 	@Override
 	public void printMenuHeader() {
-		// <write your code here>
+		System.out.println("***** CHECKOUT *****");
+		System.out.print("Enter your credit card number without spaces and press enter if you confirm purchase: ");
 	}
 
 }
