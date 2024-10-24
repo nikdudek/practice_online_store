723616c38ae08867692cb55b362be699d75c5c5a
nikdudek <nik.dudek@gmail.com>
2024-10-24 22:20:46 +0200

    Implemented DefaultCart.java

--- src/practice_online_store/enteties/impl/DefaultCart.java
+++ src/practice_online_store/enteties/impl/DefaultCart.java
@@ -1,34 +1,70 @@
 package practice_online_store.enteties.impl;
 
+import java.util.Arrays;
+
 import practice_online_store.enteties.Cart;
 import practice_online_store.enteties.Product;
 
 public class DefaultCart implements Cart {
 
-	// <write your code here>
+	private static final int DEFAULT_PRODUCT_CAPACITY = 10;
+	private Product[] products;
+	private int lastIndex;
+	
+	{
+		products = new Product[DEFAULT_PRODUCT_CAPACITY];
+	}
 	
 	@Override
 	public boolean isEmpty() {
-		// <write your code here>
+		if (products == null || products.length == 0) {
+			return true;
+		}
+		
+		for (Product product : products) {
+			if (product != null) {
+				return false;
+			}
+		}
 		
 		return true;
 	}
 
 	@Override
 	public void addProduct(Product product) {
-		// <write your code here>
+		if (product == null) {
+			return;
+		}
+		
+		if (products.length <= lastIndex) {
+			products = Arrays.copyOf(products, products.length << 1);
+		}
+		products[lastIndex++] = product;
 	}
 
 	@Override
 	public Product[] getProducts() {
-		// <write your code here>
+		int nonNullProductsAmount = 0;
+		for (Product product : products) {
+			if (product != null) {
+				nonNullProductsAmount++;
+			}
+		}
 		
-		return null;
+		Product[] nonNullProducts = new Product[nonNullProductsAmount];
+		int index = 0;
+		for (Product product : products) {
+			if (product != null) {
+				nonNullProducts[index++] = product;
+			}
+		}
+		
+		return nonNullProducts;
 	}
 
 	@Override
 	public void clear() {
-		// <write your code here>
+		products = new Product[DEFAULT_PRODUCT_CAPACITY];
 	}
 
 }
